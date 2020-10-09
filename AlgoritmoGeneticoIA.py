41183d5838dc789441207b7b213190257711e492
Marcos Danrlei <marcosdanrlei007@gmail.com>
2020-10-08 22:05:01 -0300

    Termino Da busca de Melhor Individuo

--- AlgoritmoGeneticoIA.py
+++ AlgoritmoGeneticoIA.py
@@ -2,15 +2,14 @@
 import populacao
 if __name__ == '__main__':
     objPop = populacao.Populacao(3,10,"FitnessFunction",0,6)
-    print("Indivíduos Antes da mutação:")
-    objPop.showPopulacao()
-    objPop.buscarMelhorIndviduo()
-    objPop.showMelhoresIndividuos()
+    print("Digite a quantidade de gerações:")
+    quant = input()
+    for i in range(int(quant)):
+        objPop.showPopulacao()
+        objPop.buscarMelhorIndviduo()
+        objPop.mutacaoIndividuos()
 
-    objPop.mutacaoIndividuos()
-    print("-----------------------------------------------------------------")
-    print("Indivíduos Depois da Mutação")
-    objPop.showPopulacao()
-    objPop.buscarMelhorIndviduo()
     objPop.showMelhoresIndividuos()
+    objPop.buscarMelhorTotal()
+
     
--- populacao.py
+++ populacao.py
@@ -11,16 +11,17 @@ class Populacao(object):
     # min          --> Intervalo Mínimo real da fitness
     # max          --> Intervalo Máximo real da fitness
     # melhoresInd  --> Lista de indivíduos mais adaptáveis de cada geração
-    aux = {}
+    
+    melhorTodos = {}
     
     def __init__(self, tamPop, tamInd, fitnessParam, minParam, maxParam):
         self.individuos = []
+        self.melhoresInd = []
         self.nPop = tamPop
         self.nInd = tamInd
         self.fitness = fitnessParam
         self.min = minParam
         self.max = maxParam
-        self.melhoresInd = []
         for i in range(self.nPop):
             self.individuos.append(individuo.Individuo(self.nInd,self.fitness,self.min,self.max))
             self.individuos[i].pressaoAmbiente()
@@ -34,21 +35,27 @@ class Populacao(object):
     # Visto que o intuito é minimizar (por enquanto) tem de se escolher o 
     # indivíduo que possúi menor adaptabilidade
     def buscarMelhorIndviduo(self):
-        self.aux = self.individuos[0]
+        self.melhoresInd.append(individuo.Individuo(self.nInd,self.fitness,self.min,self.max))
+        self.melhoresInd[-1].recebeDados(self.individuos[0])
         for ind in self.individuos:
-            if(ind.adaptabilidade < self.aux.adaptabilidade):
-                self.aux = ind
-        self.melhoresInd.append(self.aux)
-        print("Melhor Indivíduo")
-        for item in self.melhoresInd:
-            item.showIndividuo()
+            if(ind.adaptabilidade < self.melhoresInd[-1].adaptabilidade):
+                self.melhoresInd[-1].recebeDados(ind)
+        
 
     def mutacaoIndividuos(self):
-        for i in range(self.nPop):
-            self.individuos[i].mutacao()
+        for ind in self.individuos:
+            ind.mutacao()       
 
     def showMelhoresIndividuos(self):
         i = 1
         for ind in self.melhoresInd:
             print("Melhor Indivíduo da: ",i," Geração:");i=i+1
             ind.showIndividuo()
+
+    def buscarMelhorTotal(self):
+        self.melhorTodos = self.melhoresInd[0]
+        for ind in self.melhoresInd:
+            if(ind.adaptabilidade < self.melhorTodos.adaptabilidade):
+                self.melhorTodos.recebeDados(ind)
+        print("Melhor de Todos: ")
+        self.melhorTodos.showIndividuo()
